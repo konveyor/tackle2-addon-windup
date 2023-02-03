53936edb150b82d18d6f15bebea3e2427425b8db
David Zager <david.j.zager@gmail.com>
2023-02-03 16:00:31 -0500

    :seedling: Update e2e test to be idempotent
    This makes the e2e test idempotent and, hopefully, improves logging in
    the case of failures.
    
    Signed-off-by: David Zager <david.j.zager@gmail.com>

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -1,86 +1,59 @@
 #!/bin/bash
 
-host="${HOST:-localhost:8080/hub}"
+set -o errexit
+set -o xtrace
+
+host="${HOST:-localhost:8080}"
 
 # Port Forwarding
-kubectl port-forward service/tackle-ui 8080:8080 -n konveyor-tackle > /dev/null 2>&1 &
-pid=$!
+# kubectl port-forward service/tackle-ui 8080:8080 -n konveyor-tackle > /dev/null 2>&1 &
+# pid=$!
 
-# kill the port-forward regardless of how this script exits
-trap '{
-    kill $pid
-}' EXIT
+# Exit early if kubectl or jq not installed
+if ! command -v kubectl >/dev/null 2>&1; then
+  echo "Please install kubectl"
+  exit 1
+fi
+if ! command -v jq >/dev/null 2>&1; then
+  echo "Please install jq"
+  exit 1
+fi
 
-# wait for port to become available
-while ! nc -vz localhost 8080 > /dev/null 2>&1 ; do
-    sleep 0.1
-done
+# Verify we can talk with hub first
+if ! curl -S -s -o /dev/null -X GET ${host}/addons/windup; then
+  echo "Windup addon not found. Is the hub running?"
+  exit 1
+fi
+echo "Verified windup addon installed."
 
-# Create a Stake Holder Group
-curl -X POST ${host}/stakeholdergroups -d \
-'{
-    "name": "Big Dogs",
-    "description": "Group of big dogs."
-}' | jq -M .
-
-# Create a Stake Holder
-curl -X POST ${host}/stakeholders -d \
-'{
-    "name": "tackle",
-    "displayName":"Elmer",
-    "email": "tackle@konveyor.org",
-    "role": "Administrator",
-    "stakeholderGroups": [{"id": 1}],
-    "jobFunction" : {"id": 1}
-}' | jq -M .
-
-# Create a Business Service
-curl -X POST ${host}/businessservices -d \
-'{
-    "createUser": "tackle",
-    "name": "Marketing",
-    "Description": "Marketing Dept.",
-    "owner": {
-      "id": 1
-    }
-}' | jq -M .
-
-# Create an Application
-curl -X POST ${host}/applications -d \
-'{
-    "createUser": "tackle",
-    "name":"Pathfinder",
-    "description": "Tackle Pathfinder application.",
-    "repository": {
-      "name": "tackle-pathfinder",
-      "url": "https://github.com/konveyor/tackle-pathfinder.git",
-      "branch": "1.2.0"
-    },
-    "facts": {
-      "analysed": true
-    },
-    "businessService": {"id":1}
-}' | jq -M .
-
-# Create a Review
-curl -X POST ${host}/reviews -d \
-'{
-    "businessCriticality": 4,
-    "effortEstimate": "extra_large",
-    "proposedAction": "repurchase",
-    "workPriority": 1,
-    "comments": "This is hard.",
-    "application": {"id":1}
-}' | jq -M .
+# Create pathfinder app if it hasn't been added already
+# There is a constraint that only allows one application to have a particular name.
+if ! curl -S -s -X GET ${host}/applications | jq -e 'any(.[]; .name == "Pathfinder")'; then
+  echo "Creating pathfinder application"
+  curl -X POST ${host}/applications -d \
+    '{
+        "name":"Pathfinder",
+        "description": "Tackle Pathfinder application.",
+        "repository": {
+          "name": "tackle-pathfinder",
+          "url": "https://github.com/konveyor/tackle-pathfinder.git",
+          "branch": "1.2.0"
+        }
+    }' | jq -M .
+fi
+APP_ID=$(curl -S -s -X GET ${host}/applications | jq --raw-output '.[] | select(.name=="Pathfinder") | .id')
+echo "Pathfinder exists with id ${APP_ID}"
+# Show the applications in the inventory
+curl -S -s -X GET ${host}/applications | jq
 
 # Make a request to hub
-request_cmd="$(curl -i -o - -X POST ${host}/tasks -d \
+TASK_ID=$(curl -S -s -X POST ${host}/tasks -d \
 '{
     "name":"Windup",
     "state": "Ready",
     "locator": "windup",
     "addon": "windup",
-    "application": {"id": 1},
+    "application": {"id": '$APP_ID'},
     "data": {
         "mode": {
             "artifact": "",
@@ -107,26 +80,29 @@ request_cmd="$(curl -i -o - -X POST ${host}/tasks -d \
             "cloud-readiness"
         ]
     }
-}')"
-
-# Get status code from the curl request
-status_code="$(echo "$request_cmd" | grep HTTP | awk '{print $2}')"
-
-# Get output from the curl request
-output_response=$(echo "$request_cmd")
-echo "Output response: $output_response"
-
-# Check if status_code starts with 2
-if [[ "${status_code}" != 2* ]]; then
-    echo "Failed to create windup task"
-    echo "Got Response Status: ${status_code}"
-    exit 1
-fi
-
-pods="$(kubectl get pods -n konveyor-tackle)"
-task_status="$(echo "$pods" | grep task | awk '{print $3}')"
-if [[ $task_status != "Completed" ]]; then
-  echo "Failed to create windup task"
-  echo "Got Task Pod Status: $task_status"
+}' | jq .id)
+if [ "${TASK_ID}" = "null" ]; then
+  echo "Failed to create task"
   exit 1
 fi
+echo "Task created with id ${TASK_ID}"
+
+# Give windup ten minutes to finish
+timeout 600s bash -c "until curl -S -s -X GET ${host}/tasks/${TASK_ID} | jq -e '.state == \"Succeeded\"'; do sleep 30; done"
+
+curl -S -s -X GET ${host}/tasks/${TASK_ID} | jq
+if [ $(curl -S -s -X GET ${host}/tasks/${TASK_ID} | jq -e '.state == "Succeeded"') != "true" ]; then
+  echo "##########################################"
+  echo "Windup task did not complete successfully"
+  echo "##########################################"
+  echo "Including pod logs"
+
+  TASK_POD_NAMESPACED_NAME=$(curl -S -s -X GET ${host}/tasks/${TASK_ID} | jq --raw-output .pod)
+  TASK_POD_NAMESPACE="${TASK_POD_NAMESPACED_NAME%/*}"
+  TASK_POD_NAME="${TASK_POD_NAMESPACED_NAME#*/}"
+
+  kubectl logs --namespace ${TASK_POD_NAMESPACE} ${TASK_POD_NAME}
+  echo "Task name: ${TASK_POD_NAME} namespace: ${TASK_POD_NAMESPACE}"
+  exit 0
+fi
+echo "Windup task completed successfully"
